b07d592a57470516c59ba231c22beaa08ff8ade9
Aitkeeva Aizhan <aaitkeeva@beeline.kz>
2020-10-28 15:27:58 +0600

    api запросы CRUD

--- source/api/views.py
+++ source/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from api.serializers import QuoteCreateSerializer, QuoteUpdateSerializer, QuoteSerializer
+from webapp.models import Quote
+from .permissions import QuotePermissions
+
+class QuoteViewSet(ModelViewSet):
+    permission_classes = [QuotePermissions]
+
+    def get_queryset(self):
+        if self.request.method == 'GET' and \
+                not self.request.user.has_perm('webapp.quote_view'):
+            return Quote.get_moderated()
+        return Quote.objects.all()
+    
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return QuoteCreateSerializer
+        elif self.request.method == 'PUT':
+            return QuoteUpdateSerializer
+        return QuoteSerializer
+
+
+
